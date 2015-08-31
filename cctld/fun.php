9fee704f52c02ea25af0f12c594d11b673a0adb6
webluoye <webluoye@gmail.com>
2015-08-31 20:42:25 +0800

    分析verisign要删除的CC域名

---
+++ cctld/fun.php
@@ -0,0 +1,60 @@
+<?php
+function pinyinSplit($domainBody,$domainPinyin)
+{
+	$pyDic = explode('|', $domainPinyin);
+	$domainBody = strtolower($domainBody);
+	$len = strlen($domainBody);
+	$p = array_fill(0, $len + 1, $len);
+	$p[$len] = 0;
+	$s = array_fill(0, $len, 0);
+
+	for($i = $len - 1;$i >= 0;$i--)
+	{
+		for($j = 0,$max = $len - $i;$j < $max;$j++)
+		{
+			if($j == 0 OR array_search(substr($domainBody, $i, $j + 1), $pyDic) !== FALSE AND $p[$i + $j + 1] + 1 < $p[$i])
+			{
+				$p[$i] = $p[$i + $j + 1] + 1;
+				$s[$i] = $j + 1;
+			}
+		}
+	}
+	$tmp = 0;
+	$result = array();
+	while($tmp < $len)
+	{
+		$py = substr($domainBody, $tmp, $s[$tmp]);
+		$tmp += $s[$tmp];
+		if(array_search($py, $pyDic) !== FALSE)
+		{
+			array_push($result, $py);
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+	return $result;
+}
+
+
+function showDomain($dn,$num)
+{
+	$myDn= false;
+	if($num == 20)
+	{
+		$myDn=$dn;
+	}
+	else
+	{
+		if(strlen($dn)==$num+3)
+		{
+			$myDn=$dn;
+		}
+	}
+	if($myDn)
+	{
+		$myDn.="\n";
+	}
+	return $myDn;
+}
---
+++ cctld/index.php
@@ -0,0 +1,61 @@
+<?php
+$sm = $domainPinyin = $domainAddress = '';
+include_once('config.php');
+include_once('fun.php');
+$name =  isset($argv[1]) ?$argv[1] :false;//文件名
+$data =  isset($argv[2]) ?$argv[2] :false;//日期
+if($name==false)
+{
+	echo "how to use :\n php index.php filename date length domainType(1:纯数字,2:纯字母,3:声母4：双拼 10:all)";
+	exit;
+}
+$num =  isset($argv[3]) ?$argv[3] :20;//长度
+$strType = isset($argv[4]) ?$argv[4] :3;//是否数字 1：数字 2：字符 3 :全声母 4:双拼 10：所有
+$txt = file_get_contents($name);
+$arr = explode("\n",$txt);
+foreach($arr as $v)
+{
+	if(stripos($v,'.CC') && stripos($v, "-")===false && stripos($v, $data))
+	{
+		$dn=explode(":", $v)[0];
+		$dnBody = str_replace(".CC", "", $dn);
+		if($strType == 1 && is_numeric($dnBody))
+		{
+			echo showDomain($dn,$num);
+		}
+		if($strType == 2 && preg_match("/^[a-z]*$/i",$dnBody))
+		{
+			echo showDomain($dn,$num);
+		}
+		if($strType == 3)
+		{
+			$smArr = explode("|", $sm);
+			$strArr = str_split($dnBody);
+			$in = true;
+			foreach ($strArr as $value)
+			{
+				if(in_array(strtolower($value),$smArr)===false)
+				{
+					$in = false;
+					break;
+				}	
+			}
+			if($in)
+			{
+				echo showDomain($dn,$num);
+			}
+		}
+		if($strType == 4)
+		{
+			$py = pinyinSplit($dnBody,$domainPinyin);
+			if($py && (count($py) == $num))
+			{
+				echo $dn."\n";
+			}
+		}
+		if($strType==10)
+		{
+			echo showDomain($dn,$num);
+		}
+	}
+}
