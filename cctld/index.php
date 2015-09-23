feaefb81953b010ae2f6b849076fd765c3a2bbbd
webluoye <webluoye@gmail.com>
2015-09-23 14:53:47 +0800

    更换判断数字的函数，科学计数法也会通过

--- cctld/index.php
+++ cctld/index.php
@@ -20,7 +20,7 @@ foreach($arr as $v)
 	{
 		$dn=explode(":", $v)[0];
 		$dnBody = str_replace(".cc", "", $dn);
-		if($strType == 1 && is_numeric($dnBody))
+		if($strType == 1 && is_int($dnBody))
 		{
 			echo showDomain($dn,$num);
 		}
